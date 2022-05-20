66f52bc91bb3435ad6368cbac3d019dcdae09324
Elijah Mbugi <mukundielijah@gmail.com>
2022-05-20 08:35:40 +0300

    s-connect initial commit-react app

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-center my-10 text-blue-500 bg-red-300 p-10">
+      <h1>S-connect</h1>
+    </div>
+  );
+}
+
+export default App;
